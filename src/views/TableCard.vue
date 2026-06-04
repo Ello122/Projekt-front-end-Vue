9bcdfaa82ce18f2e8c732cd6b40d58c1e25b7ffc
KonradP14 <kondzioplacek@gmail.com>
2026-06-04 15:09:38 +0200

    widok ze zdjęciami

--- src/views/TableCard.vue
+++ src/views/TableCard.vue
@@ -6,7 +6,9 @@ const emit = defineEmits<{(e: 'open-reservation', tableId: number): void}>()
 </script>
 
 <template>
-<div class="table-card bg-sky-50 aspect-square" @click="emit('open-reservation', props.table.id)">{{ props.table.name }}
-    <img :src="props.table.image" class="w-full h-full object-cover">
+<div class="card image-full aspect-square cursor-pointer transition hover:scale-105 font-mono text-center" @click="emit('open-reservation', props.table.id)">{{ props.table.name }}
+    <figure class="m-0">
+      <img :src="props.table.image" class="w-full h-full object-cover" />
+    </figure>
 </div>
 </template>
